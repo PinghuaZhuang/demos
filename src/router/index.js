122d074a53a8e39ebddf38936380ac1f773fc294
PinghuaZhuang <zphua2016@gmail.com>
2018-04-08 02:52:04 +0800

    eslint-config-jquery

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use( Router );
 
-export default new Router({
+export default new Router( {
   routes: [
     {
       path: '/',
@@ -12,4 +12,4 @@ export default new Router({
       component: HelloWorld
     }
   ]
-})
+} );
