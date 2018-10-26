594f7c980b398c1bee81c3cfd1a60f6c59f168af
PinghuaZhuang <zphua2016@gmail.com>
2018-10-26 17:11:53 +0800

    Add: Tween - sroll

---
+++ plugins/tweenjs/js/scroll.js
@@ -0,0 +1,45 @@
+ /* eslint-disable */
+var btn = document.querySelector( '#btn' );
+var wrap = document.querySelector( '.wrap' );
+
+window.onclick = function () {
+    // var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    // console.log( scrollTop );
+}
+
+ // Setup the animation loop.
+ var coords = { x: 0, y: 0 }; // Start at (0, 0)
+ var position = { top: 0 };
+ function animate ( ) {
+     requestAnimationFrame( animate );
+     TWEEN.update( );
+ }
+ // requestAnimationFrame( animate );
+
+ var tween = new TWEEN.Tween( position ) // Create a new tween that modifies 'coords'.
+     // .to( { x: 300, y: 200 }, 1000 ) // Move to (300, 200) in 1 second.
+     .to( { top: 0 }, 500 )
+     .easing( TWEEN.Easing.Quadratic.Out ) // Use an easing function to make the animation smooth.
+     .onUpdate( function ( ret ) { // Called after tween.js updates 'coords'.
+         // Move 'box' to the position described by 'coords' with a CSS translation.
+         // box.style.setProperty( 'transform', 'translate(' + coords.x + 'px, ' + coords.y + 'px)' );
+        //  box.style.setProperty( 'transform', `rotate(${Math.round( position.position )}deg)` );
+        window.scrollTo( 0, ret.top );
+     })
+     // .start() // Start the tween immediately.
+    //  .repeat( Infinity ) // 循环动画
+     .onStop( function () {
+         console.log( position.top  );
+     } )
+     .onComplete( function () {
+         console.log( 'xxxxxx:', position.top );
+     } )
+
+btn.onclick = function () {
+    position.top = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+    animate();
+    tween.start();
+}
+
+//  animate();
+//  tween.start();
