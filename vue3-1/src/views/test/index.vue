cb7b879c2b1e5f6db23cb9aa4ee613333e503c94
PinghuaZhuang <zphua2016@gmail.com>
2019-01-04 17:50:41 +0800

    Update: vue3-1

--- vue3-1/src/views/test/index.vue
+++ vue3-1/src/views/test/index.vue
@@ -56,6 +56,9 @@ export default class TestCom extends Vue {
         // 在渲染该组件的对应路由被 confirm 前调用
         // 不！能！获取组件实例 `this`
         // 因为当守卫执行前，组件实例还没被创建
+        // 要想跳转必须调用 next 方法
+        // 跳转成功后会调用回调函数, 在回调函数里面可以使用 vm 实例
+
         // if ( to.name === 'test' ) {
         //     return
         // }
