156ed2137425e7c2f871e3f6c0921153b9779344
PinghuaZhuang <zphua2016@gmail.com>
2019-01-25 15:50:22 +0800

    Add: vue3.0 vuex plugins logger

--- vue3-1/src/store.ts
+++ vue3-1/src/store.ts
@@ -1,6 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// 内置 Logger 对象
+import createLogger from 'vuex/dist/logger'
+
+const logger = createLogger( {
+    collapsed: false, // 自动展开记录的 mutation
+    filter ( mutation, stateBefore, stateAfter ) {
+        // 若 mutation 需要被记录，就让它返回 true 即可
+        // 顺便，`mutation` 是个 { type, payload } 对象
+        return mutation.type === 'addCount'
+    },
+    transformer ( state ) {
+        // 在开始记录之前转换状态
+        // 例如，只返回指定的子树
+        return ( state as any ).count + 'xxx'
+    },
+    mutationTransformer ( mutation ) {
+        // mutation 按照 { type, payload } 格式记录
+        // 我们可以按任意方式格式化
+        return mutation.type
+    },
+    // 修改 logger 对象, 最好不要修改
+    // logger: window.console, // 自定义 console 实现，默认为 `console`
+} )
+
 Vue.use( Vuex )
 
 interface State {
@@ -43,7 +67,8 @@ var store = new Vuex.Store( {
                 }
                 console.log( 'plugins:', type, payload, state )
             } )
-        }
+        },
+        logger
     ]
 } )
 
