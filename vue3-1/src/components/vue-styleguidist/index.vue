96b79bdeff30fa0f554cdc6b036971cd03df31cb
pinghuazhuang <zphua2016@gmail.com>
2020-04-21 17:33:43 +0800

    Add.

---
+++ vue3-1/src/components/vue-styleguidist/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <el-button type="primary">测试按钮</el-button>
+</template>
+
+<script>
+export default {
+    name: 'TestBtn',
+}
+</script>
---
+++ vue3-1/styleguide.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const vueLoader = require('vue-loader')
+
+// const docSiteUrl = process.env.DEPLOY_PRIME_URL || 'https://vue-styleguidist.github.io'
+
+module.exports = {
+	components: 'src/components/**/[A-Z]*.vue',
+	defaultExample: true,
+	// template: {
+	// 	head: {
+	// 		links: [
+	// 			{
+	// 				href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+	// 				rel: 'stylesheet'
+	// 			}
+	// 		]
+	// 	}
+	// },
+	require: [
+		// path.join(__dirname, 'config/global.requires.js'),
+		// path.join(__dirname, 'config/global.styles.scss')
+	],
+	// renderRootJsx: path.join(__dirname, 'config/styleguide.root.js'),
+	validExtends: fullFilePath => !/(?=node_modules)(?!node_modules\/vuetify)/.test(fullFilePath),
+	webpackConfig: {
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader'
+				},
+				{
+					test: /\.js?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.s(c|a)ss$/,
+					use: [
+						'vue-style-loader',
+						'css-loader',
+						{
+							loader: 'sass-loader',
+							options: {
+								implementation: require('sass'),
+								sassOptions: {
+									fiber: require('fibers')
+									// indentedSyntax: true // optional
+								}
+							}
+						}
+					]
+				}
+			]
+		},
+		plugins: [new vueLoader.VueLoaderPlugin()]
+	},
+	usageMode: 'expand',
+	exampleMode: 'expand',
+	styleguideDir: 'dist',
+	ribbon: {
+		text: 'Back to examples',
+		url: `/`
+	}
+}
