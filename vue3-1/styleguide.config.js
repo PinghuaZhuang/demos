5781d9e02003c1f1e24c37ccb093226ba0a75c08
pinghuazhuang <zphua2016@gmail.com>
2020-04-21 18:51:39 +0800

    Add vue-styleguidist

--- vue3-1/styleguide.config.js
+++ vue3-1/styleguide.config.js
@@ -1,69 +1,16 @@
-const path = require('path')
-const vueLoader = require('vue-loader')
-
-// const docSiteUrl = process.env.DEPLOY_PRIME_URL || 'https://vue-styleguidist.github.io'
-
 module.exports = {
-	components: 'src/components/**/[A-Z]*.vue',
-	defaultExample: true,
-	// template: {
-	// 	head: {
-	// 		links: [
-	// 			{
-	// 				href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
-	// 				rel: 'stylesheet'
-	// 			}
-	// 		]
-	// 	}
+	// set your styleguidist configuration here
+	title: 'Default Style Guide',
+	components: 'src/components/vue-styleguidist/**/[A-Z]*.vue',
+	// defaultExample: true,
+	// sections: [
+	//   {
+	//     name: 'First Section',
+	//     components: 'src/components/**/[A-Z]*.vue'
+	//   }
+	// ],
+	// webpackConfig: {
+	//   // custom config goes here
 	// },
-	require: [
-		// path.join(__dirname, 'config/global.requires.js'),
-		// path.join(__dirname, 'config/global.styles.scss')
-	],
-	// renderRootJsx: path.join(__dirname, 'config/styleguide.root.js'),
-	validExtends: fullFilePath => !/(?=node_modules)(?!node_modules\/vuetify)/.test(fullFilePath),
-	webpackConfig: {
-		module: {
-			rules: [
-				{
-					test: /\.vue$/,
-					loader: 'vue-loader'
-				},
-				{
-					test: /\.js?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader'
-				},
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader']
-				},
-				{
-					test: /\.s(c|a)ss$/,
-					use: [
-						'vue-style-loader',
-						'css-loader',
-						{
-							loader: 'sass-loader',
-							options: {
-								implementation: require('sass'),
-								sassOptions: {
-									fiber: require('fibers')
-									// indentedSyntax: true // optional
-								}
-							}
-						}
-					]
-				}
-			]
-		},
-		plugins: [new vueLoader.VueLoaderPlugin()]
-	},
-	usageMode: 'expand',
-	exampleMode: 'expand',
-	styleguideDir: 'dist',
-	ribbon: {
-		text: 'Back to examples',
-		url: `/`
-	}
+	exampleMode: 'expand'
 }
